7d21ad2b63b055184ccd325f536240c5d059a79b
박형목(Hyungmok Park)/OCB개발팀/SKP <hyungmok.park@sk.com>
2022-08-05 14:56:43 +0900

    no message

---
+++ XCodeCloudTesting/XCodeCloudTesting/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  XCodeCloudTesting
+//
+//  Created by 박형목(Hyungmok Park)/OCB개발팀/SKP on 2022/08/05.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ XCodeCloudTesting/XCodeCloudTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  XCodeCloudTesting
+//
+//  Created by 박형목(Hyungmok Park)/OCB개발팀/SKP on 2022/08/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
